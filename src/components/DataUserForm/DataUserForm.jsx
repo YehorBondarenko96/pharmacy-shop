157af8da3e68098fba4b82ac6b7230e64473ebfd
YehorBondarenko96 <135607282+YehorBondarenko96@users.noreply.github.com>
2024-03-05 11:01:45 +0200

    3.0.0-05.03.24

--- src/components/DataUserForm/DataUserForm.jsx
+++ src/components/DataUserForm/DataUserForm.jsx
@@ -1,40 +1,67 @@
 import css from "./DataUserForm.module.css";
+import { useEffect, useRef } from "react";
 
-export const DataUserForm = () => {
+export const DataUserForm = ({ realScreenHeight }) => {
+    const realScreenWidth = window.innerWidth;
+
+
+    const allDivDUFRef = useRef(null);
+
+    useEffect(() => {
+        if (allDivDUFRef.current) {
+            const allDivDUF = allDivDUFRef.current;
+            allDivDUF.style.height = realScreenHeight - 220 + 'px';
+            allDivDUF.style.width = (realScreenWidth - 40 - realScreenWidth / 50)/2 + 'px';
+            const labelDUForms = document.querySelectorAll('.labelDUForm');
+            const pRegDUForms = document.querySelectorAll('.pRegDUForm');
+            if (labelDUForms && pRegDUForms) {
+                const coef = 1000/realScreenWidth;
+                labelDUForms.forEach(lab => {
+                    lab.style.width = realScreenWidth / (2.86 * coef) + 'px';
+                    lab.style.fontSize = (realScreenWidth > 500) ? realScreenWidth / 55.56 + 'px' : '9px';
+                    lab.style.margin = realScreenWidth / 50 + 'px';
+                    lab.style.marginRight = (realScreenWidth / 25) * coef + 'px';
+                });
+                pRegDUForms.forEach(p => {
+                    p.style.marginLeft = realScreenWidth / 100 + 'px';
+                });
+            }
+        }
+    });
     return (
-        <div className={css.allDivDUF}>
-            <label className={css.labelDUForm}>
-                    <span className={css.pRegDUForm}>Name</span>
+        <div ref={allDivDUFRef} className={css.allDivDUF}>
+            <label className={[css.labelDUForm, 'labelDUForm'].join(' ')}>
+                    <span className={[css.pRegDUForm, 'pRegDUForm'].join(' ')}>Name</span>
                 <input className={css.inputDUForm} 
                 name='name'
                 type="text"
                 autoComplete="on"
                 autoFocus
-                placeholder="Please, enter your name" />
+                placeholder={realScreenWidth > 410 ? ("Please, enter your name"):("Name")} />
             </label>
-            <label className={css.labelDUForm}>
-                    <span className={css.pRegDUForm}>Email</span>
+            <label className={[css.labelDUForm, 'labelDUForm'].join(' ')}>
+                    <span className={[css.pRegDUForm, 'pRegDUForm'].join(' ')}>Email</span>
                 <input className={css.inputDUForm} 
                 name='email'
                 type="email"
                 autoComplete="off"
-                placeholder="Please, enter your email" />
+                placeholder={realScreenWidth > 410 ? ("Please, enter your email"):("Email")} />
             </label>
-            <label className={css.labelDUForm}>
-                    <span className={css.pRegDUForm}>Phone</span>
+            <label className={[css.labelDUForm, 'labelDUForm'].join(' ')}>
+                    <span className={[css.pRegDUForm, 'pRegDUForm'].join(' ')}>Phone</span>
                 <input className={css.inputDUForm} 
                 name='phone'
                 type="number"
                 autoComplete="off"
-                placeholder="Please, enter your phone" />
+                placeholder={realScreenWidth > 410 ? ("Please, enter your phone"):("Phone")} />
             </label>
-            <label className={css.labelDUForm}>
-                    <span className={css.pRegDUForm}>Address</span>
+            <label className={[css.labelDUForm, 'labelDUForm'].join(' ')}>
+                    <span className={[css.pRegDUForm, 'pRegDUForm'].join(' ')}>Address</span>
                 <input className={css.inputDUForm} 
                 name='address'
                 type="text"
                 autoComplete="off"
-                placeholder="Please, enter your address" />
+                placeholder={realScreenWidth > 410 ? ("Please, enter your address"):("Address")} />
             </label>
         </div>
     )
--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,22 +1,52 @@
 import css from "./ShoppingCart.module.css";
 import { DataUserForm } from "../DataUserForm/DataUserForm";
 import { SavedDrugs } from "../SavedDrugs/SavedDrugs";
+import { useEffect, useRef } from "react";
 
 const ShoppingCart = () => {
     const realScreenHeight = window.innerHeight;
 
+    const divDataUsSavDrRef = useRef(null);
+    const divTotButShCRef = useRef(null);
+    const buttonShCFormRef = useRef(null);
+    const divSvgBShCRef = useRef(null);
+    const totShCRef = useRef(null);
+
+    useEffect(() => {
+        if (divDataUsSavDrRef.current&& divTotButShCRef.current && buttonShCFormRef.current &&
+            divSvgBShCRef.current && totShCRef.current) {
+            const realScreenWidth = window.innerWidth;
+            const divDataUsSavDr = divDataUsSavDrRef.current;
+            const divTotButShC = divTotButShCRef.current;
+            const buttonShCForm = buttonShCFormRef.current;
+            const divSvgBShC = divSvgBShCRef.current;
+            const totShC = totShCRef.current;
+            divDataUsSavDr.style.gap = realScreenWidth / 50 + 'px';
+            divTotButShC.style.gap = realScreenWidth / 10 + 'px';
+            divTotButShC.style.paddingTop = realScreenWidth / 100 + 'px';
+            buttonShCForm.style.width = realScreenWidth / 6.67 + 'px';
+            buttonShCForm.style.borderRadius = realScreenWidth/66.67 + 'px';
+            buttonShCForm.style.fontSize = realScreenWidth/55.556 + 'px';
+            buttonShCForm.style.gap = realScreenWidth/66.67 + 'px';
+            divSvgBShC.style.width = realScreenWidth / 33.33 + 'px';
+            divSvgBShC.style.height = realScreenWidth / 33.33 + 'px';
+            totShC.style.fontSize = realScreenWidth/41.667 + 'px';
+
+        }
+    });
+
     return (
         <>
             <form className={css.formShC}>
-                <div className={css.divDataUsSavDr}>
+                <div ref={divDataUsSavDrRef} className={css.divDataUsSavDr}>
                     <DataUserForm realScreenHeight={realScreenHeight}/>
                     <SavedDrugs realScreenHeight={realScreenHeight}/>
                 </div>
-                <div className={css.divTotButShC}>
-                        <p className={css.totShC}><b>Total price: </b></p>
-                <button className={css.buttonShCForm} type="submit">
+                <div ref={divTotButShCRef} className={css.divTotButShC}>
+                        <p ref={totShCRef} className={css.totShC}><b>Total price: </b></p>
+                <button ref={buttonShCFormRef} className={css.buttonShCForm} type="submit">
                     <p>To buy</p>
-                    <div className={css.divSvgBShC}></div>
+                    <div ref={divSvgBShCRef} className={css.divSvgBShC}></div>
                 </button>
                 </div>
             </form>
