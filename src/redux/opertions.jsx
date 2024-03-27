9f184e9a131d42287ac82d058bf8590d26d28681
YehorBondarenko96 <135607282+YehorBondarenko96@users.noreply.github.com>
2024-03-27 15:32:28 +0200

    Update opertions.jsx

--- src/redux/opertions.jsx
+++ src/redux/opertions.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-axios.defaults.baseURL = 'http://localhost:3000/api/drugs';
+axios.defaults.baseURL = 'https://pharmacy-shop-backend.onrender.com/api/drugs';
 
 export const fetchPharmacies = createAsyncThunk('pharmacies/fetchAll', async(_, thunkAPI) => {
     try{
